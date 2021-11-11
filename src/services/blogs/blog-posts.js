4cdfe9d8bd60e41660e8425e0d21addbb33a0660
Lorenzo Giorgini <lorenzo.giorgini@outlook.com>
2021-11-11 17:16:41 +0100

    Update blog-posts.js

--- src/services/blogs/blog-posts.js
+++ src/services/blogs/blog-posts.js
@@ -1,129 +1,114 @@
-import express from "express"
-import BlogPost from "../../db/modals/blogPostsModal/BlogPost.js"
-import q2m from "query-to-mongo"
+import express from "express";
+import BlogPost from "../../db/modals/blogPostsModal/BlogPost.js";
+import q2m from "query-to-mongo";
+import mongoose from "mongoose";
+const { Router } = express;
 
-const { Router } = express
-
-const router = Router()
-
+const router = Router();
 
 //Blog Posts routes
-router.route("/")
-                .get(async (req, res) => {
+router
+  .route("/")
+  .get(async (req, res) => {
+    try {
+      const mongoQuery = q2m(req.query);
 
-                    try {
-                        const mongoQuery = q2m(req.query)
+      const total = await BlogPost.countDocuments(mongoQuery.criteria);
 
-                        const total = await BlogPost.countDocuments(mongoQuery.criteria)
+      const blogPosts = await BlogPost.find(mongoQuery.criteria)
+        .limit(mongoQuery.options.limit)
+        .skip(mongoQuery.options.skip)
+        .populate({ path: "user likes", select: "name surname" });
 
-                        const blogPosts = await BlogPost.find(mongoQuery.criteria)
-                            .limit(mongoQuery.options.limit)
-                            .skip(mongoQuery.options.skip)
-                            .populate({path: "user likes" , select: "name surname"})
+      res.send({
+        links: mongoQuery.links("/blogPosts", total),
+        pageTotal: Math.ceil(total / mongoQuery.options.limit),
+        total,
+        blogPosts,
+      });
+    } catch (error) {
+      res.status(404).send({ success: false, errorr: error.message });
+    }
+  })
+  .post(async (req, res) => {
+    try {
+      const newBlogPost = new BlogPost(req.body);
 
-                        res.send({ links: mongoQuery.links("/blogPosts", total), pageTotal: Math.ceil(total / mongoQuery.options.limit), total, blogPosts })
-                        
-                    } catch (error) {
-                        res.status(404).send({success: false, errorr: error.message})
-                    }
-                })
-                .post(async (req, res) => {
+      await newBlogPost.save();
 
-                    try {
-                        const newBlogPost = new BlogPost(req.body)
+      res.status(201).send({ success: true, createdPost: newBlogPost._id });
+    } catch (error) {
+      res.status(404).send({ success: false, errorr: error.message });
+    }
+  });
 
-                        await newBlogPost.save()
+router
+  .route("/:blogId")
+  .get(async (req, res) => {
+    try {
+      const getBlogPostById = await BlogPost.findById(req.params.blogId);
 
-                        res.status(201).send({success: true, createdPost: newBlogPost._id})
+      res.status(200).send({ success: true, data: getBlogPostById });
+    } catch (error) {
+      res.status(404).send({ success: false, errorr: error.message });
+    }
+  })
+  .put(async (req, res) => {
+    try {
+      const updateBlogPostById = await BlogPost.findByIdAndUpdate(
+        req.params.blogId,
+        req.body,
+        { new: true }
+      );
 
-                    } catch (error) {
-                        res.status(404).send({success: false, errorr: error.message})
-                    }
-                })
+      res.status(200).send({ success: true, data: updateBlogPostById });
+    } catch (error) {
+      res.status(404).send({ success: false, errorr: error.message });
+    }
+  })
+  .delete(async (req, res) => {
+    try {
+      const updateBlogPostById = await BlogPost.findByIdAndDelete(
+        req.params.blogId
+      );
 
-
-router.route("/:blogId")
-                .get(async (req, res) => {
-
-                    try {
-                        const getBlogPostById = await BlogPost.findById(req.params.blogId)
-
-                        res.status(200).send({success: true, data: getBlogPostById})
-
-                    } catch (error) {
-                        res.status(404).send({success: false, errorr: error.message})
-                        
-                    }
-                })
-                .put(async (req, res) => {
-
-                    try {
-                        const updateBlogPostById = await BlogPost.findByIdAndUpdate(req.params.blogId, req.body, {new: true})
-
-                        res.status(200).send({success: true, data: updateBlogPostById})
-
-                    } catch (error) {
-                        res.status(404).send({success: false, errorr: error.message})
-
-                    }
-                })
-                .delete(async (req, res) => {
-                    try {
-                        const updateBlogPostById = await BlogPost.findByIdAndDelete(req.params.blogId)
-
-                        res.status(204).send({success: true, message: "Deleted Successfully"})
-
-                    } catch (error) {
-                        res.status(404).send({success: false, errorr: error.message})
-
-                    }
-                })
+      res.status(204).send({ success: true, message: "Deleted Successfully" });
+    } catch (error) {
+      res.status(404).send({ success: false, errorr: error.message });
+    }
+  });
 
 //Likes Route
 
-router.route("/:blogId/likes")
-                            .post(async (req, res) => {
+router.route("/:blogId/likes").post(async (req, res) => {
+  try {
+    const getBlogPost = await BlogPost.findById(req.params.blogId);
 
-                                try {
+    if (getBlogPost) {
+      const alreadyLiked = await BlogPost.findOne({
+        _id: req.params.blogId,
+        likes: new mongoose.Types.ObjectId(req.body.userId),
+      });
 
-                                    const getBlogPost = await BlogPost.findById(req.params.blogId)
-
-                                    if(getBlogPost) {
-
-                                        const alreadyLiked = getBlogPost.likes.indexOf(like => like._id.toString() === req.body.userId.toString())
-
-                                        if(alreadyLiked) {
-
-                                            getBlogPost.likes.splice(alreadyLiked, 1)
-
-                                            await getBlogPost.save()
-                                            
-                                            res.status(200).send({success: true, data: getBlogPost})
-
-                                        } else {
-
-                                            getBlogPost.likes.push(req.body.userId.toString())
-
-                                            await getBlogPost.save()
-    
-                                            res.status(200).send({success: true, data: getBlogPost})
-
-                                        }
-                                    } else {
-                                        res.status(404).send({success: false, message: "That blog post doesn't exist"})
-
-                                    }
-                                } catch (error) {
-                                    res.status(404).send({success: false, errorr: error.message})
-
-                                }
-                            })
-
-
-
-
-
-
+      if (!alreadyLiked) {
+        await BlogPost.findByIdAndUpdate(req.params.blogId, {
+          $push: { likes: req.body.userId },
+        });
+      } else {
+        await BlogPost.findByIdAndUpdate(req.params.blogId, {
+          $pull: { likes: req.body.userId },
+        });
+      }
+    } else {
+      res
+        .status(404)
+        .send({ success: false, message: "That blog post doesn't exist" });
+    }
+    res.status(200).send({ success: true, data: getBlogPost });
+  } catch (error) {
+    res.status(404).send({ success: false, errorr: error.message });
+  }
+});
 
 //Comments routes
 
@@ -135,119 +120,111 @@ PUT /blogPosts/:id/comment/:commentId => edit the comment belonging to the speci
 DELETE /blogPosts/:id/comment/:commentId=> delete the comment belonging to the specified blog post 
 */
 
+router
+  .route("/:blogPostId/comments")
+  .get(async (req, res) => {
+    try {
+      const getAllBlogPosts = await BlogPost.findById(req.params.blogPostId);
 
+      if (getAllBlogPosts) {
+        res.status(200).send({ success: true, data: getAllBlogPosts.comments });
+      } else {
+        res.status(404).send({ success: false, error: error.message });
+      }
+    } catch (error) {
+      res.status(404).send({ success: false, errorr: error.message });
+    }
+  })
+  .post(async (req, res) => {
+    try {
+      const getBlogPost = await BlogPost.findById(req.params.blogPostId);
 
+      if (getBlogPost) {
+        const comment = { ...req.body, postedAt: new Date() };
 
-router.route("/:blogPostId/comments")
-                .get(async (req, res) => {
-                    try {
-                        const getAllBlogPosts = await BlogPost.findById(req.params.blogPostId)
+        getBlogPost.comments.push(comment);
 
-                        if(getAllBlogPosts) {
+        await getBlogPost.save();
 
-                            res.status(200).send({success: true, data: getAllBlogPosts.comments})
+        res.status(201).send({ success: true, data: getBlogPost.comments });
+      } else {
+        res
+          .status(404)
+          .send({ success: false, message: "Blog Post not found" });
+      }
+    } catch (error) {
+      res.status(404).send({ success: false, errorr: error.message });
+    }
+  });
 
-                        } else {
+router
+  .route("/:blogPostId/comments/:commentId")
+  .get(async (req, res) => {
+    try {
+      const getBlogPostById = await BlogPost.findById(req.params.blogPostId);
 
-                            res.status(404).send({success: false, error: error.message})
+      if (getBlogPostById) {
+        const singleCommentById = getBlogPostById.comments.find(
+          (comment) => comment._id.toString() === req.params.commentId
+        );
 
-                        }
-                    } catch (error) {
+        if (singleCommentById) {
+          res.status(200).send({ success: true, data: singleCommentById });
+        } else {
+          res
+            .status(404)
+            .send({ success: false, message: "Comment not found" });
+        }
+      } else {
+        res.status(404).send({ success: false, message: "Comment not found" });
+      }
+    } catch (error) {
+      res.status(404).send({ success: false, errorr: error.message });
+    }
+  })
+  .put(async (req, res) => {
+    try {
+      let getBlogPostById = await BlogPost.findById(req.params.blogPostId);
 
-                        res.status(404).send({success: false, errorr: error.message})
+      if (getBlogPostById) {
+        let commentIndex = getBlogPostById.comments.findIndex(
+          (comment) => comment._id.toString() === req.params.commentId
+        );
 
-                    }
-                })
-                .post(async (req, res) => {
-                    try {
-                        const getBlogPost = await BlogPost.findById(req.params.blogPostId)
+        getBlogPostById.comments[commentIndex] = {
+          ...getBlogPostById.comments[commentIndex].toObject(),
+          ...req.body,
+          updatedAt: new Date(),
+        };
 
-                        if(getBlogPost) {
+        await getBlogPostById.save();
 
-                            const comment = {...req.body, postedAt: new Date()}
+        res.status(200).send({
+          success: true,
+          data: getBlogPostById.comments[commentIndex],
+        });
+      } else {
+        res.status(404).send({ success: false, message: "Comment not found" });
+      }
+    } catch (error) {
+      res.status(404).send({ success: false, errorr: error.message });
+    }
+  })
+  .delete(async (req, res) => {
+    try {
+      const deleteComment = await BlogPost.findByIdAndUpdate(
+        req.params.blogPostId,
+        { $pull: { comments: { _id: req.params.commentId } } },
+        { new: true }
+      );
+      if (deleteComment) {
+        res.status(204).send({ success: true, message: "Comment deleted" });
+      } else {
+        res.status(404).send({ success: false, message: "Comment Not Found" });
+      }
+    } catch (error) {
+      res.status(404).send({ success: false, errorr: error.message });
+    }
+  });
 
-                            getBlogPost.comments.push(comment)
-
-                            await getBlogPost.save()
-
-                            res.status(201).send({success: true, data: getBlogPost.comments})
-                        } else {
-                            res.status(404).send({success: false, message: "Blog Post not found"})
-                        }
-                    } catch (error) {
-                        res.status(404).send({success: false, errorr: error.message})
-                    }
-                })
-
-
-router.route("/:blogPostId/comments/:commentId")
-                .get(async (req, res) => {
-                    try {
-                        const getBlogPostById = await BlogPost.findById(req.params.blogPostId)
-
-                        if(getBlogPostById) {
-
-                            const singleCommentById = getBlogPostById.comments.find(comment => comment._id.toString() === req.params.commentId)
-
-                            if(singleCommentById) {
-                                res.status(200).send({success: true, data: singleCommentById})
-                            } else {
-                                res.status(404).send({success: false, message: "Comment not found"})
-                            }
-                        } else {
-
-                            res.status(404).send({success: false, message: "Comment not found"})
-                        }
-                    } catch (error) {
-
-                        res.status(404).send({success: false, errorr: error.message})
-
-                    }
-                })
-                .put(async (req, res) => {
-                    try {
-                        let getBlogPostById = await BlogPost.findById(req.params.blogPostId)
-
-
-                        if(getBlogPostById) {
-                            let commentIndex = getBlogPostById.comments.findIndex(comment => comment._id.toString() === req.params.commentId)
-
-                            getBlogPostById.comments[commentIndex] = {...getBlogPostById.comments[commentIndex].toObject() , ...req.body, updatedAt: new Date()}
-
-
-                            await getBlogPostById.save()
-
-                            res.status(200).send({success: true, data: getBlogPostById.comments[commentIndex]})
-                        } else {    
-                            res.status(404).send({success: false, message: "Comment not found"})
-
-                        }
-
-                    } catch (error) {
-                        res.status(404).send({success: false, errorr: error.message})
-
-                    }
-                })
-                .delete(async (req, res) => {
-                    try {
-                        const deleteComment = await BlogPost.findByIdAndUpdate(
-                            req.params.blogPostId,
-                            { $pull: { comments: { _id: req.params.commentId } } },
-                            { new: true } 
-                        )
-                        if (deleteComment) {
-                            res.status(204).send({success: true, message: "Comment deleted"})
-
-                        } else {
-                            res.status(404).send({success: false, message: "Comment Not Found"})
-
-                        }
-                    } catch (error) {
-                        res.status(404).send({success: false, errorr: error.message})
-
-                    }
-                })
-
-
-
-export default router
+export default router;
